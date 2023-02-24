b1805b5f9ca8c6e1fe7676c8ec9061395ae3ceda
Jacob Pyatt <herobbrine102@gmail.com>
2023-02-24 17:39:54 -0500

    Added feature that makes every sentance a new line New Lines still have speaker initials.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main():
     
     #Loop through the list of files
     for file in list_of_files:
+        initials = ""
         #Check if the file is a pdf and starts with "AAHP"
         if file.endswith(".pdf") and file.startswith("AAHP"):
             #Open the pdf file
@@ -36,7 +37,7 @@ def main():
             #Create a csv file with the same name as the pdf
             csvFile = open("csvs/" + file + ".csv", 'w', encoding='utf-8-sig')
             #Write header for csv file
-            csvFile.write("Text,Null copula,Person/num. agreement,Multiple negators,Existential it/dey,Perfect done,Remote past BIN,Habitual be")
+            csvFile.write("Text,Null copula,Person/num. agreement,Multiple negators,Existential it/dey,Perfect done,Remote past BIN,Habitual be\n")
             #Loop through all the pages in the pdf
             for page in pdfReader.pages:
                 #Clean the text for Rezonator
@@ -71,6 +72,33 @@ def main():
                 if pdfReader.get_page_number(page) != 1:
                     headerSpot = "Page " + str(pdfReader.get_page_number(page))
                     pageText = pageText[pageText.find(headerSpot) + len(headerSpot):]
+
+                #Add a new line and the speaker initials after every period, question mark, or exclamation point
+                count = 0
+                for i in range(len(pageText)):
+                    i += count
+                    if pageText[i] == "." or pageText[i] == "?" or pageText[i] == "!":
+                        pageText = pageText[:i + 1] + "\n" + pageText[i + 1:]
+                        count += 1
+            
+                #Make a vector of all the lines
+                lines = pageText.split("\n")
+
+                #Remove all empty lines
+                lines = [line for line in lines if line.strip() != ""]
+
+                #Loop through the lines except the first one
+                for i in range(len(lines)):
+                    #If there is a colon in the first 3 letters of the line save the initials
+                    if ":" in lines[i][:3]:
+                        initials = lines[i][:3]
+                    #If there is a colon in the line after the first 3 letters add the initials to the beginning of the line
+                    else:
+                        lines[i] = initials + lines[i]
+                    
+                
+                #Join the lines back together
+                pageText = "\n".join(lines)
                 #Extract the text from the page and write it to the csv file
                 csvFile.write(pageText)
             #Close the csv file
